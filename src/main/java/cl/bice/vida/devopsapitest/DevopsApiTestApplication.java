0fa47477c16d8d184f704ca5ea10d438e01e9131
Gustavo <gustavo_aedo@live.com>
2022-01-22 15:13:02 -0300

    Spring init file

---
+++ src/main/java/cl/bice/vida/devopsapitest/DevopsApiTestApplication.java
@@ -0,0 +1,13 @@
+package cl.bice.vida.devopsapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/bice/vida/devopsapitest/DevopsApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bice.vida.devopsapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
